8096a0c52e5ecb08ae3e2ca9e5d082ea1e64380b
Jonas Wanke <contact@jonas-wanke.com>
2019-01-26 08:40:33 +0100

    build: update jvmTarget to 1.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     application
     kotlin("jvm") version "1.3.20"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
