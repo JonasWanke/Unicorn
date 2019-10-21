77e0044516eec947c601300f15ecb60e0a784dbb
Jonas Wanke <contact@jonas-wanke.com>
2019-10-21 13:05:04 +0200

    feat: add label part to project config

--- src/main/kotlin/com/jonaswanke/unicorn/ProjectConfig.kt
+++ src/main/kotlin/com/jonaswanke/unicorn/ProjectConfig.kt
@@ -10,6 +10,7 @@ data class ProjectConfig(
     val version: SemVer = SemVer(0, 0, 1),
     val types: Types = Types(),
     val components: List<Component> = emptyList(),
+    val labels: Labels = Labels()
 ) {
     enum class Type {
         ANDROID,
@@ -42,4 +43,30 @@ data class ProjectConfig(
         val paths: List<String> = emptyList(),
         val description: String? = null
     )
+    data class Labels(
+        val components: Components = Components(),
+        val types: Types = Types(),
+        val list: List<Label> = listOf(
+            Label("duplicate", "cfd3d7"),
+            Label("wontfix", "cfd3d7"),
+            Label("discussion", "d876e3"),
+            Label("question", "d876e3")
+        )
+    ) {
+        data class Components(
+            val color: String = "c2e0c6",
+            val prefix: String = "C: "
+        )
+
+        data class Types(
+            val color: String = "c5def5",
+            val prefix: String = "T: "
+        )
+
+        data class Label(
+            val name: String,
+            val color: String = "cfd3d7",
+            val description: String? = null
+        )
+    }
 }
