998d4f6e78660777844a90bde4936f70b4a6406a
Jonas Wanke <contact@jonas-wanke.com>
2019-12-30 19:04:21 +0100

    feat(templates): support dart application packages

--- templates/dart/.template.kts
+++ templates/dart/.template.kts
@@ -1,13 +1,20 @@
 applyTemplate("base")
 
-fun TemplateRunContext.generate(name: String, isCodeGenerator: Boolean = false) {
+fun TemplateRunContext.generatePackage(
+    name: String = projectConfig.name,
+    isLibrary: Boolean = true,
+    isCodeGenerator: Boolean = false
+) {
     variables["project"] = projectConfig.copy(name = name)
     copy("pubspec.yaml.ftl")
     copy("analysis_options.yaml")
     copy(".gitignore.ftl")
-    copy("lib/package.dart.ftl", "lib/$name.dart")
+
+    if (isLibrary) copy("lib/package.dart.ftl", "lib/$name.dart")
+    else copy("lib/main.dart")
     copyDir("lib/src")
-    if (!isCodeGenerator) copyDir("example")
+
+    if (isLibrary && !isCodeGenerator) copyDir("example")
     copyDir("test")
 }
 
@@ -15,23 +22,31 @@ fun TemplateRunContext.generate(name: String, isCodeGenerator: Boolean = false) 
 variables["homepage"] = promptOptionalUrl("Homepage (for pubspec.yaml; visible on pub.dev)")
 variables["minSdkVersion"] = promptSemVer("Minimum Dart SDK version", default = "2.5.0")
 
-if (!confirm("Multiple packages in this project?", default = false)) {
-    generate(projectConfig.name)
+if (confirm("Is this an application package? (No for library packages)", default = true)) {
+    // Single application package
+    generatePackage(isLibrary = false)
 } else {
-    do {
-        val name = prompt("Please enter a package name")
-        val hasGenerator = confirm("Shall a corresponding code generator package be generated?", default = false)
-        inSubdir("name") {
-            generate(name)
-        }
-
-        if (hasGenerator) {
-            val codeGenName = "${name}_generator"
-            inSubdir(codeGenName) {
-                variables["isGenerator"] = true
-                variables["basePackage"] = name
-                generate(codeGenName, isCodeGenerator = true)
+    variables["isLibrary"] = true
+    if (!confirm("Multiple packages in this project?", default = false)) {
+        // Single library package
+        generatePackage()
+    } else {
+        // Multiple library packages
+        do {
+            val name = prompt("Please enter a package name")
+            val hasGenerator = confirm("Shall a corresponding code generator package be generated?", default = false)
+            inSubdir("name") {
+                generatePackage(name)
             }
-        }
-    } while (confirm("Add another package?", default = false))
+
+            if (hasGenerator) {
+                val codeGenName = "${name}_generator"
+                inSubdir(codeGenName) {
+                    variables["isGenerator"] = true
+                    variables["basePackage"] = name
+                    generatePackage(codeGenName, isCodeGenerator = true)
+                }
+            }
+        } while (confirm("Add another package?", default = false))
+    }
 }
