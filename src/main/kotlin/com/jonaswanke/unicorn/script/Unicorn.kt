630780ac5db2c723756768a1cb3dce44735c502d
Jonas Wanke <contact@jonas-wanke.com>
2019-12-31 16:01:43 +0100

    feat(commands): add type commands

--- src/main/kotlin/com/jonaswanke/unicorn/script/Unicorn.kt
+++ src/main/kotlin/com/jonaswanke/unicorn/script/Unicorn.kt
@@ -14,6 +14,7 @@ object Unicorn {
         registerCreateCommand()
         registerComponentCommands()
         registerPriorityCommands()
+        registerTypeCommands()
         registerTemplateCommands()
 
         // GitHub
