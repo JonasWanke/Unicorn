997428f79f9039ea8fba1b65611525f51340d9e0
Jonas Wanke <contact@jonas-wanke.com>
2020-04-01 18:23:25 +0200

    build: add custom GitHub action

---
+++ .unicorn/unicorn.kts
@@ -0,0 +1,23 @@
+import com.jonaswanke.unicorn.action.*
+import com.jonaswanke.unicorn.api.*
+import com.jonaswanke.unicorn.core.*
+import com.jonaswanke.unicorn.core.ProjectConfig.*
+import com.jonaswanke.unicorn.script.*
+import com.jonaswanke.unicorn.script.parameters.*
+import com.jonaswanke.unicorn.template.*
+import net.swiftzer.semver.SemVer
+
+unicorn {
+    gitHubAction { event ->
+        if (event !is Action.Event.PullRequest) return@gitHubAction
+
+        event.pullRequest.addAuthorAsAssignee()
+        event.pullRequest.inferLabels(this)
+
+        runChecks {
+            checkLabels()
+            checkClosedIssues()
+            checkCommitsFollowConventional()
+        }
+    }
+}
