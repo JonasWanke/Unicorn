4bf12f6821e46d46592113a1b1621c3cf2067d06
Jonas Wanke <contact@jonas-wanke.com>
2021-12-15 18:22:38 +0100

    feat(console): add release command WIP

--- .unicorn/unicorn.kts
+++ .unicorn/unicorn.kts
@@ -22,4 +22,44 @@ unicorn {
             checkCommitsFollowConventional()
         }
     }
+
+    command("release") {
+        help = "Prepare a release"
+
+        run(
+            argument("version", help = "Version of this release")
+                .semVer()
+                .optional()
+        ) { rawVersion ->
+            val mergedPrs = gitHubRepo.getMergedPrsSinceLastRelease(this)
+            val oldVersion = projectConfig.version
+            val version = rawVersion ?: Release.getNextVersionNumber(this, mergedPrs)
+
+            val branch = git.flow.createReleaseBranch(this, version)
+
+            val title = Release.titleFromVersion(this, version)
+            val changesText = Release.generateChangesText(this, mergedPrs)
+
+            log.i("Writing to changelog")
+            Files.Changelog.addRelease(this, version, title, changesText)
+
+            log.i("Changing version numbers")
+            projectConfig = projectConfig.copy(unicornVersion = version, version = version)
+            projectDir.resolve("src/main/kotlin/com/jonaswanke/unicorn/core/ProgramConfig.kt").transformLines { line ->
+                if (line.contains("val VERSION = SemVer("))
+                    with(version) {
+                        writeLine("    val VERSION = SemVer($major, $minor, $patch, ${preRelease?.let { "\"$it\"" }}, ${buildMetadata?.let { "\"$it\"" }})")
+                    }
+                else
+                    writeLine(line)
+            }
+
+            git.add(this, ".")
+            git.commit(this, "chore", description = "prepare release")
+
+//            git.push(this)
+//            val pr = gitHubRepo.createPullRequest(title, branch.name, git.flow.masterBranch.name, changesText)
+//            Desktop.getDesktop().browse(pr.htmlUrl.toURI())
+        }
+    }
 }
--- src/main/kotlin/com/jonaswanke/unicorn/console/Main.kt
+++ src/main/kotlin/com/jonaswanke/unicorn/console/Main.kt
@@ -2,11 +2,70 @@ package com.jonaswanke.unicorn.console
 
 import com.jonaswanke.unicorn.core.readScript
 import com.jonaswanke.unicorn.script.Unicorn
-import com.jonaswanke.unicorn.script.unicorn
-import java.security.Permission
+
 
 fun main(args: Array<String>) {
+// Debug
+//    if (System.console() == null)
+//        System.setSecurityManager(object : SecurityManager() {
+//            override fun checkExit(status: Int) {
+//                throw SecurityException()
+//            }
+//
+//            override fun checkPermission(perm: Permission?) = Unit
+//        })
+
+//    unicorn {
+//                execute("./gradlew", "distZip")
+
+//                    Files.Changelog.addRelease(version, name, )
+//
+//                    val release = gitHub.createRelease(version, tagName = "") {
+//                        name = null
+//                        date = Date()
+//                        preRelease = false
+//
+//                        description = promptOptional("Enter a description")
+//                        changelog = changelogFormatter {
+//                            branch.newPrs
+//                                .sort(...)
+//                            .group(...)
+//                            .joinToString("\n") {
+//                            "- *${it.components.joinToString(", ")}:* ${it.name} (#${it.id})"
+//                            + ", fixes ${it.fixedIssues.joinToString(", ") { "#${it.id}" }}"
+//                        }
+//                        }
+//
+//                        binaries += File("./build/distributions")  // Matcher
+//                    }
+
+//                    git.commit(ConventionalCommit.Type.CHORE, description = "bump version")
+//
+//                    val ghRelease = branch.release(release)
+//
+//                    Bintray.publishVersion(
+//                        release = release,
+//                        name = release.name,
+//                        released = ghRelease.publishedAt,
+//                        desc = release.description,
+//                        githubReleaseNotesFile = Files.changelog.path,
+//                        githubUseTagReleaseNotes = true,
+//                        vcsTag = release.tagName
+//                    ) {
+//                        addFile(..., listInDownloads = true)
+//                    }
+//    }
+//        Unicorn.main(listOf("release", "0.2.1", "--prefix=testRepo 1"))
+
+//        Unicorn.main(listOf("login"))
+//        Unicorn.main(listOf("create", "testRepo 1"))
+//        Unicorn.main(listOf("-h"))
+//        Unicorn.main(listOf("issue", "assign", "-h"))
+//        Unicorn.main(listOf("issue", "assign", "1", "--prefix=testRepo 1"))
+//        Unicorn.main(listOf("issue", "complete", "fix config", "--prefix=testRepo 1"))
+//    } else {
     readScript()
     Unicorn.main(args.asList())
+//    }
 }
 
